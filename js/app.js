d6f51f7a8aa4e1a010e14accd0baf74507b82a5a
kilianthoraval <kthoraval@gmail.com>
2022-10-13 17:49:41 +0200

    p3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 
+
+let numberOfDice = parseInt(prompt("Combien de dés veux tu ?"));
+
 // div, class
 
 let divElem = document.createElement("div");
@@ -12,27 +15,37 @@ function getRandomDice(min, max) {
     let random = Math.round(Math.random() * (max - min) + min);    
     return random;    
 }
-
 let dice = getRandomDice(1,6)
 
+function howMany() {
+    
+    numberOfDice = dice;
+    
+}
+
+
+howMany()
 // play
 
-if (dice === 1){
-    divElem.style.backgroundPositionX = "0px"; 
-}
-else if (dice === 2){
-    divElem.style.backgroundPositionX = "100px";
-}
-else if (dice === 3){
-    divElem.style.backgroundPositionX = "200px";
-}
-else if (dice === 4){
-    divElem.style.backgroundPositionX = "300px";
-}
-else if (dice === 5){
-    divElem.style.backgroundPositionX = "400px";
-}
-else if (dice === 6){
-    divElem.style.backgroundPositionX = "500px";
-}
+    if (dice === 1){
+        divElem.style.backgroundPositionX = "0px"; 
+    }
+    else if (dice === 2){
+        divElem.style.backgroundPositionX = "100px";
+    }
+    else if (dice === 3){
+        divElem.style.backgroundPositionX = "200px";
+    }
+    else if (dice === 4){
+        divElem.style.backgroundPositionX = "300px";
+    }
+    else if (dice === 5){
+        divElem.style.backgroundPositionX = "400px";
+    }
+    else if (dice === 6){
+        divElem.style.backgroundPositionX = "500px";
+    }
+
+
+
 
