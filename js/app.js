53d398f063cce3bc7ab78221956c90069e4c7225
kilianthoraval <kthoraval@gmail.com>
2022-10-13 17:17:08 +0200

    p2

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+// div, class
+
+let divElem = document.createElement("div");
+divElem.classList.add("dice");
+let parentElem = document.getElementById("player");
+parentElem.appendChild(divElem);
+
+// fonction
+
+function getRandomDice(min, max) {
+    let random = Math.round(Math.random() * (max - min) + min);    
+    return random;    
+}
+
+let dice = getRandomDice(1,6)
+
+// play
+
+if (dice === 1){
+    divElem.style.backgroundPositionX = "0px"; 
+}
+else if (dice === 2){
+    divElem.style.backgroundPositionX = "100px";
+}
+else if (dice === 3){
+    divElem.style.backgroundPositionX = "200px";
+}
+else if (dice === 4){
+    divElem.style.backgroundPositionX = "300px";
+}
+else if (dice === 5){
+    divElem.style.backgroundPositionX = "400px";
+}
+else if (dice === 6){
+    divElem.style.backgroundPositionX = "500px";
+}
+
